8fec009c64158c551b88d80e9a236e7eeb019ce2
김민서 <blackcode-@naver.com>
2023-09-27 13:17:24 +0900

    Read replica 구성 테스트

--- MySQL/src/main/java/com/kbhc/blackcode/Service/DataServiceImp.java
+++ MySQL/src/main/java/com/kbhc/blackcode/Service/DataServiceImp.java
@@ -2,14 +2,13 @@ package com.kbhc.blackcode.Service;
 
 import java.sql.Timestamp;
 
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kbhc.blackcode.Mapper.DataMapper;
 import com.kbhc.blackcode.VO.DataVO;
@@ -18,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@Transactional
 public class DataServiceImp implements DataService {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -27,27 +27,31 @@ public class DataServiceImp implements DataService {
      * - Autowired 는 권장되지 않기에 생성자 주입 형태로 구성합니다.
      */
 	private final SqlSession sqlSession;
+	private final SqlSession masterSqlSession;
+	private final SqlSession slaveSqlSession;
 //	private final DataSource master;
 //	private final DataSource slave;
 	
-	public DataServiceImp(SqlSession sqlSession) {
+	public DataServiceImp(SqlSession sqlSession,
+			@Qualifier("masterSqlSession") SqlSession masterSqlSession,
+			@Qualifier("slaveSqlSession") SqlSession slaveSqlSession) {
 		super();
 		this.sqlSession = sqlSession;
-//		this.master = master;
-//		this.slave = slave;
+		this.masterSqlSession = masterSqlSession;
+		this.slaveSqlSession = slaveSqlSession;
 	}
 	
 	/**
 	 * 1초에 2번 Auto Insert
 	 */
 	int count = 0 ;
-	@Scheduled(fixedDelay = 500, zone = "Asia/Seoul")
+	@Scheduled(fixedDelay = 1000, zone = "Asia/Seoul")
 	public void insertData() {
 		DataVO dataVO = new DataVO();
 		count ++;
 		dataVO.setDate(new Timestamp(System.currentTimeMillis()));
 		dataVO.setContents(Integer.toString(count));
-		DataMapper dm = sqlSession.getMapper(DataMapper.class);
+		DataMapper dm = masterSqlSession.getMapper(DataMapper.class);
 		try {
 			dm.insertData(dataVO);
 		} catch (Exception e) {
@@ -60,9 +64,10 @@ public class DataServiceImp implements DataService {
 	/**
 	 * 1초에 1번 Auto Select
 	 */
-	@Scheduled(fixedDelay = 1000, zone = "Asia/Seoul")
+	@Transactional(readOnly = true)
+	@Scheduled(fixedDelay = 500, zone = "Asia/Seoul")
 	public void selectCountData() {
-		DataMapper dm = sqlSession.getMapper(DataMapper.class);
+		DataMapper dm = slaveSqlSession.getMapper(DataMapper.class);
 		logger.info("TOTAL : "+dm.selectCountData());
 	}
 
